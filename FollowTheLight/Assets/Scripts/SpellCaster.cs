b34aecb4a994605d65fa413ce9f9cd92c120670c
Vamiyn <vamiynr@gmail.com>
2015-10-29 22:43:06 +0200

    the very first steps for the prototype

---
+++ FollowTheLight/Assets/Scripts/SpellCaster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellCaster : MonoBehaviour {
+	GameObject orb;
+
+	void Start () {
+		orb = (GameObject)Resources.Load ("LightOrb");
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			GameObject spawnedOrb = (GameObject)Instantiate (orb, gameObject.transform.position + gameObject.transform.rotation * new Vector3(0, 1, 1), Quaternion.identity);
+			spawnedOrb.transform.parent = gameObject.transform;
+		}
+	}
+}
