3c577431723d88b7daed8b896e1677d08ebc3557
Vamiyn <vamiynr@gmail.com>
2015-10-30 19:49:11 +0200

    playing with the orb casting

---
+++ FollowTheLight/Assets/Scripts/OrbLife.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbLife : MonoBehaviour {
+
+	public bool released;
+
+	public float suicideTimer;
+	public bool maxLifeTime;
+	public float maxSize;
+
+	GameObject playerObject;
+	Rigidbody rb;
+	SpellCaster scscript;
+	Light lighting;
+
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+		lighting = gameObject.GetComponent<Light>();
+		playerObject = GameObject.Find ("Player");
+		scscript = playerObject.GetComponent<SpellCaster>();
+		released = false;
+	}
+
+	void Update () {
+		growIfNotReleased ();
+	}
+
+	public void releaseOrb() {
+		released = true;
+		rb.constraints = RigidbodyConstraints.None;
+		rb.velocity = playerObject.transform.rotation * new Vector3 (0, 2.0f, 8.0f);
+		gameObject.transform.parent = null;
+	}
+
+	public void startSuicide() {
+		StartCoroutine (suicideTiming());
+	}
+
+	IEnumerator suicideTiming() {
+		while (suicideTimer > 0.0f) {
+			yield return new WaitForSeconds(1);
+			suicideTimer -= 1.0f;
+		}
+		suicide ();
+	}
+
+	void suicide() {
+		Destroy (gameObject);
+	}
+
+	void lifeTimeChecker() {
+
+	}
+
+	void growIfNotReleased() {
+		if (!released) {
+			if (gameObject.transform.localScale.x >= maxSize) {
+				gameObject.transform.localScale = new Vector3 (maxSize, maxSize, maxSize);
+				lighting.range = maxSize/2;
+			} else {
+				gameObject.transform.localScale += new Vector3 (1.0f, 1.0f, 1.0f) * Time.deltaTime;
+				lighting.range += ((0.5f * Time.deltaTime));
+			}
+		}
+	}
+}
--- FollowTheLight/Assets/Scripts/SpellCaster.cs
+++ FollowTheLight/Assets/Scripts/SpellCaster.cs
@@ -1,17 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SpellCaster : MonoBehaviour {
+
+	public int orbLimit;
+	List<GameObject> orbList;
+
 	GameObject orb;
+	Rigidbody rbPlayer;
+
+	GameObject spawnedOrb;
+	Rigidbody rbOrb;
 
 	void Start () {
+		rbPlayer = gameObject.GetComponent<Rigidbody> ();
 		orb = (GameObject)Resources.Load ("LightOrb");
+		orbList = new List<GameObject> ();
 	}
 
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Alpha1)) {
-			GameObject spawnedOrb = (GameObject)Instantiate (orb, gameObject.transform.position + gameObject.transform.rotation * new Vector3(0, 1, 1), Quaternion.identity);
-			spawnedOrb.transform.parent = gameObject.transform;
+			startCastingOrb();
+		}
+		if (Input.GetKeyUp(KeyCode.Alpha1)) {
+			spawnedOrb.GetComponent<OrbLife>().releaseOrb();
+		}
+
+		checkOrbLimits ();
+	}
+
+	void startCastingOrb() {
+		spawnedOrb = (GameObject)Instantiate (orb, gameObject.transform.position + gameObject.transform.rotation * new Vector3(0, 1.0f, 1.5f), Quaternion.identity);
+		spawnedOrb.transform.parent = gameObject.transform;
+		registerOrb (spawnedOrb);
+		rbOrb = spawnedOrb.GetComponent<Rigidbody>();
+	}
+
+	void releaseOrb() {
+		spawnedOrb.GetComponent<OrbLife> ().released = true;
+		rbOrb.constraints = RigidbodyConstraints.None;
+		rbOrb.velocity = rbPlayer.rotation * new Vector3 (0, 2.0f, 8.0f);
+		spawnedOrb.transform.parent = null;
+	}
+
+	public void registerOrb(GameObject orb) {
+		orbList.Add(orb);
+	}
+
+	public void removeOrbFromList(GameObject orb) {
+		orbList.Add(orb);
+	}
+
+	void checkOrbLimits() {
+		if (orbList.Count > orbLimit) {
+			GameObject oldestOrb = orbList[0];
+			orbList.RemoveAt(0);
+			oldestOrb.GetComponent<OrbLife>().startSuicide();
+			
 		}
 	}
 }
