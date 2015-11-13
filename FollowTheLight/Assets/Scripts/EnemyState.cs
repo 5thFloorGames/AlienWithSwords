b248b5943b149673dd6224df7d931834af948dd1
Vamiyn <vamiynr@gmail.com>
2015-11-13 16:11:44 +0200

    enemy healthbars are updated according to the damage they take

--- FollowTheLight/Assets/Scripts/EnemyState.cs
+++ FollowTheLight/Assets/Scripts/EnemyState.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class EnemyState : MonoBehaviour {
 
 	public bool dead;
-	public int health;
+	public int currentHealth;
+	Image healthMeter;
 
 	int maximumHealth;
 
 	public void Init(int amount) {
 		dead = false;
 		maximumHealth = amount;
-		health = amount;
-		//updateHealthToUI ();
+		currentHealth = amount;
+		healthMeter = transform.FindChild("EnemyInfo").FindChild("HealthMeter").GetComponent<Image>();
+		updateHealthToHealthMeter ();
 	}
 	
 	void Start () {
@@ -25,15 +28,19 @@ public class EnemyState : MonoBehaviour {
 
 	void takeDamage(int amount) {
 		if (!dead) {
-			Debug.Log(gameObject.name + " took " +  amount + " damage");
-			health -= amount;
-			if (health <= 0) {
-				health = 0;
-				Debug.Log (gameObject.name + " is dead :(");
+			//Debug.Log(gameObject.name + " took " +  amount + " damage");
+			currentHealth -= amount;
+			if (currentHealth <= 0) {
+				currentHealth = 0;
+				//Debug.Log (gameObject.name + " is dead :(");
 				dead = true;
 				Destroy (gameObject);
 			}
-			//updateHealthToUI ();
+			updateHealthToHealthMeter ();
 		}
+	}
+
+	void updateHealthToHealthMeter() {
+		healthMeter.fillAmount = ((float)currentHealth/maximumHealth);
 	}
 }
