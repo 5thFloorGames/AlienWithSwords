157a65ef37b44ee6e497f1d4b730fcaa28b86bf4
Vamiyn <vamiynr@gmail.com>
2015-12-09 16:05:38 +0200

    enemyshot files added

---
+++ FollowTheLight/Assets/Scripts/EnemyShotDamages.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShotDamages : MonoBehaviour {
+
+    int damage;
+    float delayFromEnemy;
+    float lifeTime;
+
+    public void Init(int damageAmount, float delay, float lifetimeFromEnemy) {
+
+        delayFromEnemy = delay;
+        lifeTime = lifetimeFromEnemy;
+        damage = damageAmount;
+    }
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player" && (other.GetType() == typeof(CapsuleCollider))) {
+            other.gameObject.SendMessageUpwards("TakeDamage", damage);
+        }
+        CreateHitEffect();
+        Destroy(gameObject);
+    }
+
+    void CreateHitEffect() {
+        GameObject prefab = (GameObject)Resources.Load("BulletHitEffect");
+        Instantiate(prefab, transform.position + (transform.rotation * new Vector3(0, 0, -0.5f)), Quaternion.identity);
+    }
+}
