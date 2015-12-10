c423d28f9c38d99c70a792c3c1b27f8cadcf1137
Vamiyn <vamiynr@gmail.com>
2015-12-10 15:27:36 +0200

    started adding sounds

---
+++ FollowTheLight/Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSoundController : MonoBehaviour {
+
+	[SerializeField] AudioSource quoteSource;
+	[SerializeField] AudioSource effectSource;
+	 
+	[SerializeField] AudioClip[] selectCharacterQuotes;
+	[SerializeField] AudioClip[] attackingQuotes;
+	[SerializeField] AudioClip[] killsAnEnemyQuotes;
+
+	[SerializeField] AudioClip dyingQuote;
+	[SerializeField] AudioClip killsACharacterQuote;
+
+	[SerializeField] AudioClip[] attackSoundEffects;
+
+	
+	void Start () {
+		//source = GetComponent<AudioSource>();
+	}
+
+	void Update () {
+	
+	}
+
+
+	// Playing the Quotes
+
+	public void PlaySelectionQuote() {
+		if (selectCharacterQuotes.Length == 0) {
+			return;
+		}
+		int n = Random.Range (1, selectCharacterQuotes.Length);
+		quoteSource.clip = selectCharacterQuotes[n];
+		quoteSource.PlayOneShot(quoteSource.clip);
+		if (selectCharacterQuotes.Length > 1) {
+			selectCharacterQuotes [n] = selectCharacterQuotes [0];
+			selectCharacterQuotes [0] = quoteSource.clip;
+		}
+	}
+}
--- FollowTheLight/Assets/Scripts/CharacterState.cs
+++ FollowTheLight/Assets/Scripts/CharacterState.cs
@@ -8,17 +8,22 @@ public class CharacterState : MonoBehaviour {
     public bool dead;
 
     int health;
+	bool inCharacter;
+	bool delayedDone;
 
     CharacterManager cm;
 	UserInterfaceManager uim;
 
+	CharacterSoundController cas;
 	AudioListener audioListener;
+	AudioSource controllerSoundSource;
 	GameObject characterCamera;
 	GameObject sprite;
 	SpriteRenderer spriteRenderer;
 
 	void Awake() {
 		audioListener = GetComponentInChildren<AudioListener> ();
+		controllerSoundSource = GetComponent<AudioSource> ();
 		characterCamera = transform.FindChild("Camera").gameObject;
 		sprite = transform.FindChild ("Sprite").gameObject;
 		spriteRenderer = sprite.GetComponent<SpriteRenderer> ();
@@ -28,21 +33,39 @@ public class CharacterState : MonoBehaviour {
 	public void Init(GameObject manager) {
         cm = manager.GetComponent<CharacterManager>();
 		uim = GameObject.Find ("UserInterface").GetComponent<UserInterfaceManager>();
+		cas = GetComponentInChildren<CharacterSoundController>();
 		health = maximumHealth;
 		UpdateHealthToUI ();
 	}
 
 	public void EnterCharacter() {
+		inCharacter = true;
 		audioListener.enabled = true;
         characterCamera.SetActive(true);
 		sprite.SetActive(false);
+		controllerSoundSource.mute = true;
+		cas.PlaySelectionQuote ();
+		if (delayedDone) {
+			controllerSoundSource.mute = false;
+		} else {
+			StartCoroutine (DelayedUnmute ());
+			delayedDone = true;
+		}
 	}
 
 	public void LeaveCharacter() {
+		inCharacter = false;
 		audioListener.enabled = false;
         characterCamera.SetActive(false);
 		if (!dead) {
         	sprite.SetActive(true);
+		}
+	}
+
+	IEnumerator DelayedUnmute() {
+		yield return new WaitForSeconds (0.5f);
+		if (inCharacter) {
+			controllerSoundSource.mute = false;
 		}
 	}
 
