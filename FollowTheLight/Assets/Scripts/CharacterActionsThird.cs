a2f474029a5910d84f0615391b75bfb117c1e020
Vamiyn <vamiynr@gmail.com>
2015-12-10 19:41:02 +0200

    character3 targeting feedback correct

--- FollowTheLight/Assets/Scripts/CharacterActionsThird.cs
+++ FollowTheLight/Assets/Scripts/CharacterActionsThird.cs
@@ -162,6 +162,7 @@ public class CharacterActionsThird : MonoBehaviour {
             }
             aimedCharacter.SendMessage("NotAimedAt");
             characterInAim = false;
+			aimedCharacter = null;
 		}
 	}
 	
