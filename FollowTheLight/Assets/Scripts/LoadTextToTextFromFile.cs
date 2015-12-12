ff751254271639dda1b39381dbaa91c5aa81224b
Vamiyn <vamiynr@gmail.com>
2015-12-13 01:51:41 +0200

    useragreementtext added, ready to be integrated into the game

---
+++ FollowTheLight/Assets/Scripts/LoadTextToTextFromFile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Collections;
+
+public class LoadTextToTextFromFile : MonoBehaviour {
+
+    public string filename;
+
+    Text text;
+
+	void Start () {
+        if (filename != "" && System.IO.File.Exists(filename)) {
+            text = GetComponent<Text>();
+            text.text = System.IO.File.ReadAllText(filename);
+        }
+	}
+
+	void Update () {
+	
+	}
+}
