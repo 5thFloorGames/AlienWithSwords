e316ba7d26f6d108f1d9cf6d4e2d8cea676ca684
Vamiyn <vamiynr@gmail.com>
2015-12-12 18:14:40 +0200

    joseph damage dealing works properly now

---
+++ FollowTheLight/Assets/Scripts/HealerDamageCollisionDetector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealerDamageCollisionDetector : MonoBehaviour {
+
+    LaserController lc;
+
+	void Start () {
+        lc = transform.parent.parent.FindChild("Laser").GetComponent<LaserController>();
+	}
+
+    void OnParticleCollision(GameObject other) {
+        lc.AttackParticlesCollided();
+    }
+}
