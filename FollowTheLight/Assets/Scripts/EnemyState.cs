e0d81fb0d9584e39e33287176489470a67c19f84
Vamiyn <vamiynr@gmail.com>
2015-12-14 02:22:11 +0200

    let's not have the blood floating over the enemy

--- FollowTheLight/Assets/Scripts/EnemyState.cs
+++ FollowTheLight/Assets/Scripts/EnemyState.cs
@@ -138,7 +138,7 @@ public class EnemyState : MonoBehaviour {
         Invoke("ColliderDeactivate", 1.0f);
 		GameObject prefab = (GameObject) Resources.Load("BloodPool");
 		transform.FindChild ("EnemyInfo").gameObject.SetActive (false);
-		Instantiate (prefab, transform.position + new Vector3(0, 0.2f, 0), Quaternion.identity);
+		Instantiate (prefab, transform.position, Quaternion.identity);
 	}
 
     void ColliderDeactivate() {
